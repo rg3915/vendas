4a8c6481f18496e6934b6a68e377c803ab1993e5
rg3915 <rg3915@yahoo.com.br>
2014-11-24 23:01:30 -0200

    dj_static 0.0.6

--- vendas_project/wsgi.py
+++ vendas_project/wsgi.py
@@ -1,8 +1,8 @@
+# coding: utf-8
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendas_project.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
