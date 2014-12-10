d8d25bb7a980ee6cb6b58c741c68586ab6fc37f1
rg3915 <rg3915@yahoo.com.br>
2014-12-10 05:39:33 -0200

    humanize format numbers

--- vendas_project/settings.py
+++ vendas_project/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'django_extensions',
     'vendas_project.vendas',
 )
