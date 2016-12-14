c125081bd412d4ee7e66f6aedec59516f40efbd1
rg3915 <rg3915@yahoo.com.br>
2016-12-13 22:17:11 -0200

    Refactor create sales

--- fixtures/create_sales.py
+++ fixtures/create_sales.py
@@ -1,16 +1,16 @@
-import random
-from core.models import Customer, Seller, Sale, SaleDetail, Product
+from random import randint
+from vendas.core.models import Customer, Seller, Sale, SaleDetail, Product
 from fixtures.gen_random_values import *
 
-REPEAT = 200
+REPEAT = 120
 qcustomers = Customer.objects.count()
 qsellers = Seller.objects.count()
 qproducts = Product.objects.count()
 
 for i in range(REPEAT):
-    c = random.randint(1, qcustomers)
+    c = randint(1, qcustomers)
     customer = Customer.objects.get(pk=c)
-    s = random.randint(1, qsellers)
+    s = randint(1, qsellers)
     seller = Seller.objects.get(pk=s)
     obj = Sale(
         customer=customer,
@@ -18,11 +18,11 @@ for i in range(REPEAT):
     )
     obj.save()
     print(obj.pk)
-    for j in range(random.randint(1, 10)):
+    for j in range(randint(1, 10)):
         sale = Sale.objects.get(pk=obj.pk)
-        p = random.randint(1, qproducts)
+        p = randint(1, qproducts)
         product = Product.objects.get(pk=p)
-        quantity = random.randint(1, 50)
+        quantity = randint(1, 50)
         price_sale = product.price
         ipi_sale = product.ipi
         sd = SaleDetail(
