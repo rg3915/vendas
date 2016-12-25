41c00aeb805601f9a532dfcf35ddf46062952e73
rg3915 <rg3915@yahoo.com.br>
2016-12-25 01:54:34 -0200

    Aplicando novo estilo em core/urls.py

--- vendas/core/urls.py
+++ vendas/core/urls.py
@@ -2,27 +2,67 @@ from django.conf.urls import include, url
 from vendas.core import views as c
 
 customer_patterns = [
-    url(r'^$', c.CustomerList.as_view(), name='customer_list'),
-    url(r'^(?P<pk>\d+)/$', c.CustomerDetail.as_view(), name='customer_detail'),
-    url(r'^edit/(?P<pk>\d+)/$', c.CustomerUpdate.as_view(), name='customer_update'),
+    url(
+        regex=r'^$',
+        view=c.CustomerList.as_view(),
+        name='customer_list'
+    ),
+    url(
+        regex=r'^(?P<pk>\d+)/$',
+        view=c.CustomerDetail.as_view(),
+        name='customer_detail'
+    ),
+    url(
+        regex=r'^edit/(?P<pk>\d+)/$',
+        view=c.CustomerUpdate.as_view(),
+        name='customer_update'
+    ),
 ]
 brand_patterns = [
-    url(r'^$', c.BrandList.as_view(), name='brand_list'),
+    url(
+        regex=r'^$',
+        view=c.BrandList.as_view(),
+        name='brand_list'
+    ),
 ]
 
 product_patterns = [
-    url(r'^$', c.ProductList.as_view(), name='product_list'),
+    url(
+        regex=r'^$',
+        view=c.ProductList.as_view(),
+        name='product_list'
+    ),
 ]
 
 sale_patterns = [
-    url(r'^add/$', c.sale_create, name='sale_add'),
-    url(r'^$', c.SaleList.as_view(), name='sale_list'),
-    url(r'^(?P<pk>\d+)/$', c.SaleDetailView.as_view(), name='sale_detail'),
+    url(
+        regex=r'^add/$',
+        view=c.sale_create,
+        name='sale_add'
+    ),
+    url(
+        regex=r'^$',
+        view=c.SaleList.as_view(),
+        name='sale_list'
+    ),
+    url(
+        regex=r'^(?P<pk>\d+)/$',
+        view=c.SaleDetailView.as_view(),
+        name='sale_detail'
+    ),
 ]
 
 seller_patterns = [
-    url(r'^$', c.SellerList.as_view(), name='seller_list'),
-    url(r'^(?P<pk>\d+)/$', c.SellerDetail.as_view(), name='seller_detail'),
+    url(
+        regex=r'^$',
+        view=c.SellerList.as_view(),
+        name='seller_list'
+    ),
+    url(
+        regex=r'^(?P<pk>\d+)/$',
+        view=c.SellerDetail.as_view(),
+        name='seller_detail'
+    ),
 ]
 
 urlpatterns = [
